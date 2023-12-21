c4320ee332a3b0f75511b968eb4a82d134859fb3
Martin Stransky <stransky@anakreon.cz>
2023-12-21 10:54:47 +0100

    Initial release

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#include "wayland-proxy.h"
+#include <cstdio>
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("Wayland proxy load balancer, run as:\n\n%s application_name\n\n", argv[0]);
+    return 0;
+  }
+  auto proxy = WaylandProxy::Create();
+  if (!proxy) {
+    return 1;
+  }
+  proxy->SetVerbose(true);
+  return !proxy->RunChildApplication(argv+1);
+}
